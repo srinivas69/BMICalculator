61376760577d14aa19131c80b71e0cc5892b1309
srinivas69 <srinivas@ybn.co.in>
2014-08-06 01:34:59 +0530

    mentioned forumla for BMI calculator.

--- app/src/main/java/com/seenu/bmicalculator/app/MainActivity.java
+++ app/src/main/java/com/seenu/bmicalculator/app/MainActivity.java
@@ -2,16 +2,37 @@ package com.seenu.bmicalculator.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements TextWatcher {
+    private EditText wtEt;
+    private EditText htEt;
+    private TextView resultTv;
+
+    private double wt = 0.0;
+    private double ht = 0.0;
+    private double result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wtEt = (EditText)findViewById(R.id.editText);
+        htEt = (EditText)findViewById(R.id.editText2);
+        resultTv = (TextView)findViewById(R.id.textView2);
+
+        wtEt.addTextChangedListener(this);
+        htEt.addTextChangedListener(this);
     }
 
 
@@ -35,4 +56,49 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+        String wtStr = wtEt.getText().toString();
+        String htStr = htEt.getText().toString();
+
+        if(wtStr.equals(""))
+            wt = 0.0;
+        else
+            wt = Double.parseDouble(wtStr);
+
+        if(htStr.equals(""))
+            ht = 0.0;
+        else
+            ht = Double.parseDouble(htStr);
+
+        result = wt/(ht*ht);
+     //   result = round(result,2);
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+        String resultStr = String.format("%.2f", result);
+        System.out.println(resultStr);
+        if(resultStr.equals("Infinity"))
+            resultTv.setText("");
+        else
+            resultTv.setText(resultStr);
+    }
+
+    private double round(double value, int places){
+
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
 }
